1fac71978929e2f0293b1734c2dacdf378c13632
Luiz Augusto Oliveira <luiz.augusto.farias@ccc.ufcg.edu.br>
2025-09-16 16:21:10 -0300

    features

---
+++ functions.php
@@ -0,0 +1,76 @@
+<?php
+function handel_add_woocommerce_support() {
+  add_theme_support('woocommerce');
+}
+add_action('after_setup_theme', 'handel_add_woocommerce_support');
+
+function handel_css() {
+  wp_register_style('handel-style', get_template_directory_uri() . '/style.css', [], '1.0.0', false);
+  wp_enqueue_style('handel-style');
+}
+add_action('wp_enqueue_scripts', 'handel_css');
+
+function handel_custom_images() {
+  add_image_size('slide', 1000, 800, ['center', 'top']);
+  update_option('medium_crop', 1);
+}
+add_action('after_setup_theme', 'handel_custom_images');
+
+function handel_loop_shop_per_page() {
+  return 6;
+}
+add_filter('loop_shop_per_page', 'handel_loop_shop_per_page');
+
+function remove_some_body_class($classes) {
+  $woo_class = array_search('woocommerce', $classes);
+  $woopage_class = array_search('woocommerce-page', $classes);
+  $search = in_array('archive', $classes) || in_array('product-template-default', $classes);
+  if($woo_class && $woopage_class && $search) {
+    unset($classes[$woo_class]);
+    unset($classes[$woopage_class]);
+  }
+  return $classes;
+}
+add_filter('body_class', 'remove_some_body_class');
+
+include(get_template_directory() . '/inc/product-list.php');
+
+add_filter('woocommerce_enable_order_notes_field', '__return_false');
+
+function meu_dashboard() {
+  echo 'Origamid';
+}
+
+add_action('woocommerce_account_dashboard', 'meu_dashboard');
+
+include(get_template_directory() . '/inc/user-custom-menu.php');
+include(get_template_directory() . '/inc/checkout-customizado.php');
+
+
+// function handel_change_email_header() {
+//   echo '<h2 style="text-align: center;">Mensagem Header</h2>';
+// }
+
+// add_action('woocommerce_email_header', 'handel_change_email_header');
+
+function handel_change_email_footer_text($text) {
+  echo 'Handel
+  <ul style="padding: 0px; margin: 0px; list-style: none;">
+    <li><a href="/">Facebook</a></li>
+    <li><a href="/">Instagram</a></li>
+    <li><a href="/">YouTube</a></li>
+  </ul>';
+}
+add_filter('woocommerce_email_footer_text', 'handel_change_email_footer_text');
+
+function handel_add_email_meta($order) {
+  $mensagem = get_post_meta($order->get_id(), 'mensagem_personalizada', true);
+  $presente = get_post_meta($order->get_id(), '_billing_presente', true);
+  
+  echo '<h2 style="margin: -20px 0 10px 0px;">Detalhes</h2>
+    <p style="font-size: 16px; border: 1px solid #e5e5e5; padding: 10px; margin-bottom: 0px;"><strong>Mensagem: </strong>' . $mensagem .  '</p>
+    <p style="font-size: 16px; border: 1px solid #e5e5e5; padding: 10px;"><strong>Presente: </strong>' . $presente .  '</p>
+  ';
+}
+add_action('woocommerce_email_order_meta', 'handel_add_email_meta');
+?>
